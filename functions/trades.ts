a63ff9c98078d880f6a161c10cb053b6abba9669
Brandon <brandon@alphadao.money>
2022-02-04 11:05:12 +0000

    Add files via upload

---
+++ functions/trades.ts
@@ -0,0 +1,52 @@
+import { getAddress } from '@ethersproject/address'
+import { Handler } from "@netlify/functions"
+import BigNumber from 'bignumber.js'
+import { getSwaps } from './_shared'
+import { createSuccessResponse, createBadRequestResponse, createServerErrorResponse } from './utils/response'
+
+export const handler: Handler = async event => {
+  if (!event.queryStringParameters?.pair || !/^0x[0-9a-fA-F]{40}_0x[0-9a-fA-F]{40}$/.test(event.queryStringParameters.pair)) {
+    return createBadRequestResponse('Invalid pair identifier: must be of format tokenAddress_tokenAddress')
+  }
+
+  const [tokenA, tokenB] = event.queryStringParameters.pair.split('_')
+  let idA: string, idB: string
+  try {
+    ;[idA, idB] = [getAddress(tokenA), getAddress(tokenB)]
+  } catch (error) {
+    return createBadRequestResponse('Invalid pair identifier: both asset identifiers must be *checksummed* addresses')
+  }
+
+  try {
+    const swaps = await getSwaps(idA, idB)
+
+    return createSuccessResponse(
+      swaps.map(swap => {
+        const aIn = swap.amountAIn !== '0'
+        const aOut = swap.amountAOut !== '0'
+        const bIn = swap.amountBIn !== '0'
+        const bOut = swap.amountBOut !== '0'
+
+        // a is the base so if the pair sends a and not b then it's a 'buy'
+        const isBuy = aOut && bIn && !aIn && !bOut
+        const isSell = !aOut && !bIn && aIn && bOut
+        const isBorrowBoth = aOut && bOut && aIn && bIn
+
+        const type = isBuy ? 'buy' : isSell ? 'sell' : isBorrowBoth ? 'borrow-both' : '???'
+        const baseAmount = aOut ? swap.amountAOut : swap.amountAIn
+        const quoteAmount = bOut ? swap.amountBOut : swap.amountBIn
+        return {
+          trade_id: swap.id,
+          base_volume: baseAmount,
+          quote_volume: quoteAmount,
+          type,
+          trade_timestamp: swap.timestamp,
+          price:
+            baseAmount !== '0' ? new BigNumber(quoteAmount).dividedBy(new BigNumber(baseAmount)).toString() : undefined
+        }
+      })
+    )
+  } catch (error) {
+    return createServerErrorResponse(error)
+  }
+}
